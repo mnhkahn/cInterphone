3f03ba084da2551e19ac51550788f2589f3f6246
Bryce <lichao0407@gmail.com>
2014-06-11 19:45:35 +0800

    datapicker

--- src/com/cyeam/cInterphone/ui/ProcessActivity.java
+++ src/com/cyeam/cInterphone/ui/ProcessActivity.java
@@ -1,13 +1,21 @@
 package com.cyeam.cInterphone.ui;
 
-import com.cyeam.cInterphone.R;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TimePicker;
 import android.widget.Toast;
+
+import com.cyeam.cInterphone.R;
 
 public class ProcessActivity extends Activity {
 	private int processid;
+	private TimePicker tp;
+	private EditText content;
+	private ImageButton upload;
+	private ImageButton delete;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +23,27 @@ public class ProcessActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.dialog_process);
 		
+		tp = (TimePicker) findViewById(R.id.tpPicker);
+		tp.setIs24HourView(true);
+		
 		processid = getIntent().getIntExtra("processid", -1);
 		Toast.makeText(this, "" + processid,
 				Toast.LENGTH_SHORT).show();
+		
+		upload = (ImageButton) findViewById(R.id.process_upload);
+		delete = (ImageButton) findViewById(R.id.process_delete);
+		content = (EditText) findViewById(R.id.process_content_dialog);
+		
+		upload.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				int t = tp.getCurrentHour() * 3600 + tp.getCurrentMinute() * 60; 
+				Toast.makeText(v.getContext(), content.getText().toString() + t,
+						Toast.LENGTH_SHORT).show();
+			}
+		});
+		
 	}
 	
 
